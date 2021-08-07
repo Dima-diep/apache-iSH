e78acbc8e33ffbbc7dba385a7aa261b88c8d3f2e
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-08 01:06:02 +0400

    change.py

--- change.py
+++ change.py
@@ -9,8 +9,8 @@ print("3.joe")
 a = int(input())
 
 if a == 1:
-    os.system("apt install nano && nano /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
+    os.system("apk add nano && nano /usr/etc/apache2/httpd.conf")
 elif a == 2:
-    os.system("apt install micro && clear && echo "Ctrl-S for save, Ctrl-Q for exit" && sleep 10s && micro /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
+    os.system("apk add micro && clear && echo "Ctrl-S for save, Ctrl-Q for exit" && sleep 10s && micro /usr/etc/apache2/httpd.conf")
 elif a == 3:
-    os.system("apt install joe && clear && echo "Ctrl-K-Q for exit" && sleep 10s && joe /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
+    os.system("apk add joe && clear && echo "Ctrl-K-Q for exit" && sleep 10s && joe /usr/etc/apache2/httpd.conf")
